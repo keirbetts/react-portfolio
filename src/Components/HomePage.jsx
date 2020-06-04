827bc240301b3461aa48b619ef77ec01841e5805
keirbetts <keirjbetts@gmail.com>
2020-06-04 12:50:53 +0100

    aa

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -7,35 +7,35 @@ import Typing from 'react-typing-animation'
 class HomePage extends Component {
   render() {
     return (
-      <div style={{ width: '100%' }}>
-        <Grid className='home-grid'>
-          <Cell col={12}>
-            <div className='banner-text'>
-              {/* <Typing> */}
+      // <div style={{ width: '100%' }}>
+      <Grid className='home-grid'>
+        <Cell col={12}>
+          <div className='banner-text'>
+            <Typing>
               <div>
-                {/* <Typing.Delay ms={1000} /> */}
+                <Typing.Delay ms={1000} />
                 <h1>Full Stack Developer</h1>
               </div>
-              {/* </Typing> */}
-              {/* <Typing > */}
+            </Typing>
+            <Typing >
               <div>
-                {/* <Typing.Delay ms={1000} /> */}
+                <Typing.Delay ms={1000} />
                 <h2 style={{ color: '#00FF00' }}>......................
               </h2>
               </div>
-              {/* </Typing> */}
-              {/* <Typing speed={100}> */}
+            </Typing>
+            <Typing speed={100}>
               <div>
-                {/* <Typing.Delay ms={2500} />
-                  <Typing.Speed ms={5} /> */}
+                <Typing.Delay ms={2500} />
+                <Typing.Speed ms={5} />
                 <h3>React | React Native | JavaScript |  NodeJS | Express | Python | HTML/CSS</h3>
               </div>
-              {/* </Typing > */}
-            </div>
-          </Cell>
-        </Grid>
+            </Typing >
+          </div>
+        </Cell>
+      </Grid>
 
-      </div >
+      // </div >
     );
   }
 }
