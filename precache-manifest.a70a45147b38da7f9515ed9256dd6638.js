ca0dcc2ac514c61baf516839c7d80aff4dba3bfb
keirbetts <keirjbetts@gmail.com>
2020-06-04 12:18:30 +0100

    Updates

---
+++ precache-manifest.a70a45147b38da7f9515ed9256dd6638.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e25b58a7216d93e7b06ac29d2f99fb9",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "5a65be4c484e421435a7",
+    "url": "/react-portfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "4caa1ff6cc63dbd265e3",
+    "url": "/react-portfolio/static/css/main.7f807899.chunk.css"
+  },
+  {
+    "revision": "5a65be4c484e421435a7",
+    "url": "/react-portfolio/static/js/2.7c02b960.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/react-portfolio/static/js/2.7c02b960.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4caa1ff6cc63dbd265e3",
+    "url": "/react-portfolio/static/js/main.60136735.chunk.js"
+  },
+  {
+    "revision": "893a6b40a8a028ca2ea3",
+    "url": "/react-portfolio/static/js/runtime-main.e412fa6e.js"
+  },
+  {
+    "revision": "c8aa131a278d50cec0b6d5124dda5ca6",
+    "url": "/react-portfolio/static/media/cambo.c8aa131a.jpg"
+  },
+  {
+    "revision": "571599e1dd6a2e0561959cf85763f2a0",
+    "url": "/react-portfolio/static/media/camboSchool.571599e1.jpg"
+  },
+  {
+    "revision": "89a570c5da5cd2603800b2ad19b53fd0",
+    "url": "/react-portfolio/static/media/camboTeam.89a570c5.jpg"
+  },
+  {
+    "revision": "c58237cd4fda7baeaf81cd629ee190c4",
+    "url": "/react-portfolio/static/media/cardiffLogo.c58237cd.png"
+  },
+  {
+    "revision": "f79e8b632537f6230ecacaae3e970c0e",
+    "url": "/react-portfolio/static/media/dance.f79e8b63.jpg"
+  },
+  {
+    "revision": "94d96a327b6d71216317a2bd3194f609",
+    "url": "/react-portfolio/static/media/keirBetts.94d96a32.png"
+  },
+  {
+    "revision": "243f922509e2be7798031e56a2243970",
+    "url": "/react-portfolio/static/media/vsoLogo.243f9225.png"
+  }
+]);
